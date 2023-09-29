c04a11ff9735c26e4d660531c972e76a73c4ef8c
Richkum <kumatum2@gmail.com>
2023-09-29 09:06:21 +0100

    finishing

--- asset/script/palin.js
+++ asset/script/palin.js
@@ -1,18 +1,17 @@
 const printt = document.getElementById('print')
 const fin = document.getElementById('finish')
 
-function palindrom(){
-  let enter = document.getElementById('second').value
+function palindrom () {
+  const enter = document.getElementById('second').value
 
-  let result = " "
+  let result = ' '
 
-  result = enter.split('').reverse().join('');
+  result = enter.split('').reverse().join('')
 
-  if(enter === result){
+  if (enter === result) {
     fin.textContent = 'This is a palindrom'
     console.log('This is a palindrom')
-  }
-  else{
+  } else {
     fin.textContent = 'This is not a palindrom'
     console.log('This is not a palindrom')
   }
